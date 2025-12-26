4de8d0c4e22f82716ad3283e7fd4e5d8df1578c2
dkagh012 <5064664@naver.com>
2025-12-26 13:55:57 +0900

    테스트트

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -3,16 +3,13 @@
 import styles from "./Header.module.scss";
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/images/logo.png";
+import logo from "@/app/images/logo.png";
 export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <Link href="/" className={styles.logo}>
-                    <img
-                        src={logo}
-                        alt="GESUN"
-                    />
+                    <img src={logo.src} alt="GESUN" />
                 </Link>
                 <Link href="/estimate" className={styles.link}>
                     클린업 견적 요청
